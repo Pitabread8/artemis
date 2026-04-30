89eab57ff322924d3a059d2dafaae63760b078dc
Sita Vemuri <sitavemuri8@gmail.com>
2026-04-29 22:58:09 -0400

    gh pages files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://pitabread8.github.io',
+    base: '/artemis',
+});
